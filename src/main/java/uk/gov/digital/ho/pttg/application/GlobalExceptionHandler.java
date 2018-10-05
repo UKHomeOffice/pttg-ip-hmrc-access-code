b264c1e08ae3e3e560f820c04e7d761d791e4de9
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2018-10-05 13:15:47 +0100

    EE-7488 Remove unnecessary qualifier

--- src/main/java/uk/gov/digital/ho/pttg/application/GlobalExceptionHandler.java
+++ src/main/java/uk/gov/digital/ho/pttg/application/GlobalExceptionHandler.java
@@ -38,31 +38,31 @@ class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(BAD_REQUEST);
     }
 
-    @ExceptionHandler(value = ApplicationExceptions.HmrcUnauthorisedException.class)
+    @ExceptionHandler(ApplicationExceptions.HmrcUnauthorisedException.class)
     public ResponseEntity handle(ApplicationExceptions.HmrcUnauthorisedException e) {
         log.info("HmrcUnauthorisedException: {}", e.getMessage(), value(EVENT, HMRC_ACCESS_CODE_AUTHENTICATION_ERROR));
         return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
-    @ExceptionHandler(value = ApplicationExceptions.ProxyForbiddenException.class)
+    @ExceptionHandler(ApplicationExceptions.ProxyForbiddenException.class)
     public ResponseEntity handle(ApplicationExceptions.ProxyForbiddenException e) {
         log.error("Received 403 Forbidden from a request to HMRC. This was from the proxy and not HMRC.", value(EVENT, HMRC_PROXY_ERROR));
         return new ResponseEntity<>(e.getMessage(), INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(value = HttpServerErrorException.class)
+    @ExceptionHandler(HttpServerErrorException.class)
     public ResponseEntity handle(HttpServerErrorException e) {
         log.error("HttpServerErrorException: {}\nError response body: {}", e.getMessage(), e.getResponseBodyAsString(), value(EVENT, HMRC_ACCESS_CODE_RESPONSE_ERROR));
         return new ResponseEntity<>(e.getMessage(), e.getStatusCode());
     }
 
-    @ExceptionHandler(value = RestClientException.class)
+    @ExceptionHandler(RestClientException.class)
     public ResponseEntity handle(RestClientException e) {
         log.error("RestClientException: {}", e.getMessage(), value(EVENT, HMRC_ACCESS_CODE_RESPONSE_ERROR));
         return new ResponseEntity<>(e.getMessage(), INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(value = Exception.class)
+    @ExceptionHandler(Exception.class)
     public ResponseEntity handle(Exception e) {
         log.error("Fault Detected:", e, value(EVENT, HMRC_ACCESS_CODE_RESPONSE_ERROR));
         return new ResponseEntity<>(e.getMessage(), INTERNAL_SERVER_ERROR);
