414da9c33178e7112dffe6c07d17157f2f934be8
Tim-Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2020-05-13 10:53:04 +0100

    Ee 31813 (#75)
    * EE-31813: Supply the client-secret to HMRC for Access Code generation.
    
    * EE-31813: Whitespace change.
    
    * EE-31813: Improve variable name.
    
    * EE-31813: Whitespace change to force drone build.

--- src/main/java/uk/gov/digital/ho/pttg/hmrc/HmrcClient.java
+++ src/main/java/uk/gov/digital/ho/pttg/hmrc/HmrcClient.java
@@ -22,58 +22,60 @@ import static uk.gov.digital.ho.pttg.application.ApplicationExceptions.HmrcAcces
 import static uk.gov.digital.ho.pttg.application.LogEvent.EVENT;
 import static uk.gov.digital.ho.pttg.application.LogEvent.HMRC_GET_ACCESS_CODE_SUCCESS;
 
-
 @Component
 @Slf4j
 public class HmrcClient {
 
     private final RestTemplate restTemplate;
     private final String clientId;
+    private final String clientSecret;
     private final String accessTokenResource;
 
     @Autowired
     HmrcClient(@Qualifier(value = "hmrcRestTemplate") RestTemplate restTemplate,
-                      @Value("${client.id}") String clientId,
-                      @Value("${hmrc.endpoint}") String baseHmrcUrl) {
+               @Value("${client.id}") String clientId,
+               @Value("${client.secret}") String clientSecret,
+               @Value("${hmrc.endpoint}") String baseHmrcUrl) {
         this.restTemplate = restTemplate;
         this.clientId = clientId;
+        this.clientSecret = clientSecret;
         this.accessTokenResource = baseHmrcUrl + "/oauth/token";
     }
 
     public AccessCodeHmrc getAccessCodeFromHmrc(String totpCode) {
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(APPLICATION_FORM_URLENCODED);
-
         AccessCodeHmrc accessCode;
 
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-
-        map.add("grant_type", "client_credentials");
-        map.add("client_id", clientId);
-        map.add("client_secret", totpCode);
-
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
-
-        log.debug("Calling HMRC for new access code");
-
         try {
-            accessCode = restTemplate.postForEntity(accessTokenResource, request, AccessCodeHmrc.class).getBody();
-            if(accessCode == null) {
+            accessCode = restTemplate.postForEntity(accessTokenResource, generateRequest(totpCode), AccessCodeHmrc.class).getBody();
+            if (accessCode == null) {
                 throw new HmrcAccessCodeServiceRuntimeException("HMRC returned null access code");
             }
         } catch (HttpClientErrorException ex) {
             HttpStatus statusCode = ex.getStatusCode();
-           if (statusCode.equals(FORBIDDEN)) {
+            if (statusCode.equals(FORBIDDEN)) {
                 throw new ApplicationExceptions.ProxyForbiddenException("Received a 403 Forbidden response from proxy");
             } else if (statusCode.equals(UNAUTHORIZED)) {
                 throw new ApplicationExceptions.HmrcUnauthorisedException(ex.getMessage(), ex);
             } else {
-               throw new HmrcAccessCodeServiceRuntimeException("Problem retrieving Access Code from HMRC", ex);
+                throw new HmrcAccessCodeServiceRuntimeException("Problem retrieving Access Code from HMRC", ex);
             }
         }
 
         log.info("Received access code response", value(EVENT, HMRC_GET_ACCESS_CODE_SUCCESS));
         return accessCode;
+    }
+
+    private HttpEntity<MultiValueMap<String, String>> generateRequest(String totpCode) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> oauthData = new LinkedMultiValueMap<>();
+
+        oauthData.add("grant_type", "client_credentials");
+        oauthData.add("client_id", clientId);
+        oauthData.add("client_secret", totpCode + clientSecret);
+
+        return new HttpEntity<>(oauthData, headers);
     }
 }
